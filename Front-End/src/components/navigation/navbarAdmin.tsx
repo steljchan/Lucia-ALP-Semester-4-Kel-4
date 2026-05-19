80e38d345e9782fcb83689b479cbb5bd34122ac3
Arsya Aulia <arsya1522@gmail.com>
2026-05-19 16:24:53 +0800

    delete front-end lama

--- Front-End/src/components/navigation/navbarAdmin.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import NavbarBaseAdmin from './NavbarAdminBase';
-
-export default function NavbarAdmin(props: any) {
-  return <NavbarBaseAdmin {...props} />;
-}
--- Front-End/src/services/authservices.ts
+++
@@ -1,7 +0,0 @@
-// Front-End/src/services/authservice.ts
-import { auth } from "../config/firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
-
-export const login = (email: any, password: any) => {
-  return signInWithEmailAndPassword(auth, email, password);
-};
