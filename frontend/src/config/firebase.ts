1674b8f448a8ba4eeabae51fa297542820818b93
Arsya Aulia <arsya1522@gmail.com>
2026-05-30 18:30:29 +0800

    perbaikan fitur upload materi dan login firebase

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -1,26 +1,36 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp} from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getFunctions } from "firebase/functions";
-import { getAuth } from "firebase/auth";
+// @ts-ignore
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const firebaseConfig = {
   apiKey: "AIzaSyDZTLeHhEnoJmx40LTzgaMeQtR0wbiYGQA",
   authDomain: "lucia-4b190.firebaseapp.com",
   projectId: "lucia-4b190",
-  storageBucket: "lucia-4b190.appspot.com",
+  storageBucket: "lucia-4b190.firebasestorage.app", 
   messagingSenderId: "172224433843",
   appId: "1:172224433843:web:90d6b38565e4cfd3766ac6",
 };
 
-const app = initializeApp(firebaseConfig);
+const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-export const auth = getAuth(app);
+let firebaseAuth;
+try {
+  firebaseAuth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage),
+  });
+} catch (error) {
+ 
+  const { getAuth } = require("firebase/auth");
+  firebaseAuth = getAuth(app);
+}
 
+export const auth = firebaseAuth;
 export const db = getFirestore(app);
-
 export const storage = getStorage(app);
-
 export const functions = getFunctions(app, "us-central1");
 
 export default app;
